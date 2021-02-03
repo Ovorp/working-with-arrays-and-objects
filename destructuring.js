1dc0fe1be36f1bbd69bb7e9b7cbddc0e54ff7a85
Nathaniel Shirts <nshirts7@gmail.com>
2021-02-02 22:07:52 -0700

    finished the destructuring problems

--- destructuring.js
+++ destructuring.js
@@ -35,7 +35,7 @@ const {color, make, model, year} = carDetails;
 
 function greeting( obj ) {
   //Code Here
-  
+  const {firstName, lastName, title} = obj;
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -54,7 +54,10 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+const totalPopulation = obj => {
+  const {utah, california, texas, arizona} = obj;
+  return utah + california + texas + arizona;
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -69,7 +72,12 @@ function greeting( obj ) {
 
 //Code Here
 
-
+const ingredients = obj => {
+  const {carb, fat, protein} = obj;
+  let arr = [];
+  arr.push(carb, fat, protein);
+  return arr;
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -87,7 +95,13 @@ function greeting( obj ) {
 
 //Code Here
 
-
+const largeNumbers = ({first, second, third}) => {
+  if (first < second && first < third) {
+    return first;
+  } else if (second < first && second < third) {
+    return second;
+  } else return third;
+}
 
 ////////// PROBLEM 6 //////////
 
@@ -98,5 +112,11 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+const numberGroups = ({a, b, c}) => {
+  if (a.length > b.length && a.length > c.length) {
+    return a;
+  } else if (b.length > a.length && b.length > c.length) {
+    return b;
+  } else return c;
+}
 
