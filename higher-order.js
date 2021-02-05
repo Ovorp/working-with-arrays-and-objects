2cde551d223c1577af63ed2830cb35b88ca0ca06
Nathaniel Shirts <nshirts7@gmail.com>
2021-02-04 20:51:08 -0700

    additional solution for problem 6

--- higher-order.js
+++ higher-order.js
@@ -124,6 +124,6 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal = purchases.filter(val => val.owner === 'Bob').reduce((total, val) => total + val.price, 0)
-//let bobsTotal = purchases.reduce((total, val) => (val.owner === 'Bob')? total + val.price : total + 0, 0)
+// let bobsTotal = purchases.filter(val => val.owner === 'Bob').reduce((total, val) => total + val.price, 0)
+let bobsTotal = purchases.reduce((total, val) => (val.owner === 'Bob')? total + val.price : total + 0, 0)
 
